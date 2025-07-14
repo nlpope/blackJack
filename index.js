e12efe49a8f33e3635f19e5319bb6207d447f610
Noah Pope <npope@tutanota.com>
2025-07-13 22:09:52 -0500

    adding method to calculate cards

--- index.js
+++ index.js
@@ -9,5 +9,17 @@ function getRandomIntInclusive(min, max)
     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
 
+
+function calculateCards(sum)
+{
+    if (sum < 21) {
+        console.log("Do ou want to draw a new card? 🙂")
+    } else if (sum === 21) {
+        console.log("Woohoo! You've got Blackjack! 🥳")
+    } else if (sume > 21) {
+        console.log("You're out.  othe game! 😭")
+    }
+}
+
 console.log(firstCard, secondCard)
 console.log(sum)
