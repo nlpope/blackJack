85ec4feb8c3d31fa2c431c91b3c3656b8e5ab620
Noah Pope <npope@tutanota.com>
2025-07-13 23:09:26 -0500

    adding type checking and age verification

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
-let firstCard = getRandomIntInclusive(2, 11)
-let secondCard = getRandomIntInclusive(2, 11)
-let sum = firstCard + secondCard
+let admitted = true
+
+
+function validateAge(age)
+{
+    if (!Number.isFinite(age)) { console.log("age must be a number"); admitted = false; return }
+    if (age < 21) { 
+        admitted = false
+        console.log("You can not enter the club!")
+    } else { 
+        admitted = true
+        console.log("Welcome!")
+    }
+}
+
+
+function calculateCards()
+{
+    if (!admitted) { return }
+
+    let firstCard = getRandomIntInclusive(2, 11)
+    let secondCard = getRandomIntInclusive(2, 11)
+    let sum = firstCard + secondCard
+
+    console.log(firstCard, secondCard)
+    if (sum < 21) {
+        console.log("Do you want to draw a new card? 🙂")
+    } else if (sum === 21) {
+        console.log("Woohoo! You've got Blackjack! 🥳")
+    } else {
+        console.log("You're out of the game! 😭")
+    }
+}
+
+/**
+-------------------------------------------------------------
+MARKER: BACKGROUND METHODS
+ */
 
 function getRandomIntInclusive(min, max)
 {
@@ -9,17 +44,5 @@ function getRandomIntInclusive(min, max)
     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
 
-
-function calculateCards(sum)
-{
-    if (sum < 21) {
-        console.log("Do ou want to draw a new card? 🙂")
-    } else if (sum === 21) {
-        console.log("Woohoo! You've got Blackjack! 🥳")
-    } else if (sume > 21) {
-        console.log("You're out.  othe game! 😭")
-    }
-}
-
-console.log(firstCard, secondCard)
-console.log(sum)
+validateAge("20")
+calculateCards()
