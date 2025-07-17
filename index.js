479da4d40ca0888d04ce4fc1c82665bb174a86aa
Noah Pope <npope@tutanota.com>
2025-07-17 07:03:03 -0500

    changing message on screen based on age & card results

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 let admitted = false
 let isAlive = true
 let hasBlackjack = false
+let message = ""
+let messageEl = document.getElementById("message-el")
 
 
 function startGame()
 {
-    validateAge()
+    validateAge(21)
+    calculateCards()
 }
 
 
@@ -14,11 +17,13 @@ function validateAge(age)
     if (!Number.isFinite(age)) { console.log("age must be a number"); admitted = false; return }
     if (age < 21) { 
         admitted = false
-        console.log("You can not enter the club!")
+        message = "You can not enter the club!"
     } else { 
         admitted = true
-        console.log("Welcome!")
+        message = "Welcome!"
     }
+
+    messageEl.textContent = message
 }
 
 
@@ -32,15 +37,16 @@ function calculateCards()
 
     console.log(firstCard, secondCard)
     if (sum <= 20) {
-        console.log("Do you want to draw a new card? 🙂")
+        message = "Do you want to draw a new card? 🙂"
     } else if (sum === 21) {
-        console.log("Woohoo! You've got Blackjack! 🥳")
+        message = "Woohoo! You've got Blackjack! 🥳"
         hasBlackjack = true
     } else {
         isAlive = false
-        console.log("You're out of the game! 😭")
-        console.log("isAlive = " + isAlive)
+        message = "You're out of the game! 😭"
     }
+            
+    messageEl.textContent = message
 }
 
 /**
@@ -54,6 +60,3 @@ function getRandomIntInclusive(min, max)
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
-
-validateAge(21)
-calculateCards()
