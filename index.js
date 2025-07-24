4b0922b6c1c2b0e708ff51c7c834992f2d6a8c0b
Noah Pope <npope@tutanota.com>
2025-07-24 06:09:35 -0500

    displaying cards to screen

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ let isAlive = true
 let hasBlackjack = false
 let message = ""
 let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 
 function startGame()
 {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
     validateAge(21)
     calculateCards()
 }
@@ -47,7 +49,7 @@ function calculateCards()
         message = "You're out of the game!"
     }
             
-    sumEl.textContent = sum
+    sumEl.textContent = "Sum: " + sum
     messageEl.textContent = message
 }
 
